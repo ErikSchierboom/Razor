903f0e9aaf17da73a0f5c5b0dfca0ba34dd968f1
N. Taylor Mullen <nimullen@microsoft.com>
2014-12-01 15:39:29 -0800

    Re-design TagHelperOutput and runtime dependencies to allow all content modes.
    - Added PreContent, PostContent and ContentSet properties to TagHelperOutput.
    - Added GeneratePreContent, GeneratePostContent and SupressOutput methods to TagHelperOutput.
    - Added multile ExecuteChildContentAsync and GetChildContentAsync to the rendering phase, ultimately only exposing GetChildContentAsync to a TagHelper author.
    - Added more knowledge of StartWritingScope and EndWritingScope to the TagHelper runtime components. This is to enable the runtime components to utilize the RazorPage's infrastructure to render a delegate to a writer and retrieve its value to ultimately expose it to the user.
    
    #221

--- src/Microsoft.AspNet.Razor.Runtime/TagHelpers/TagHelperContext.cs
+++ src/Microsoft.AspNet.Razor.Runtime/TagHelpers/TagHelperContext.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Microsoft.AspNet.Razor.Runtime.TagHelpers
 {
@@ -14,14 +16,26 @@ namespace Microsoft.AspNet.Razor.Runtime.TagHelpers
         /// Instantiates a new <see cref="TagHelperContext"/>.
         /// </summary>
         /// <param name="allAttributes">Every attribute associated with the current HTML element.</param>
-        public TagHelperContext([NotNull] IDictionary<string, object> allAttributes)
+        /// <param name="getChildContentAsync">A delegate used to execute and retrieve the rendered child content 
+        /// asynchronously.</param>
+        public TagHelperContext([NotNull] IDictionary<string, object> allAttributes,
+                                [NotNull] Func<Task<string>> getChildContentAsync)
         {
             AllAttributes = allAttributes;
+            GetChildContentAsync = getChildContentAsync;
         }
 
         /// <summary>
         /// Every attribute associated with the current HTML element.
         /// </summary>
-        public IDictionary<string, object> AllAttributes { get; private set; }
+        public IDictionary<string, object> AllAttributes { get; }
+
+        /// <summary>
+        /// A delegate used to execute and retrieve the rendered child content asynchronously.
+        /// </summary>
+        /// <remarks>
+        /// Child content is only executed once. Successive calls to this delegate return a cached result.
+        /// </remarks>
+        public Func<Task<string>> GetChildContentAsync { get; }
     }
 }
